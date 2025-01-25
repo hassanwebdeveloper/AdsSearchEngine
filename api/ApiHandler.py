8472f60f9349cfc093680d65c4255164754ffa1b
hassanwebdeveloper <h.baig34@gmail.com>
2025-01-25 23:31:20 +0500

    Base flask plus react app
    Base flask plus react app

---
+++ api/ApiHandler.py
@@ -0,0 +1,14 @@
+from flask_restful import Api, Resource, reqparse
+
+
+
+class ApiHandler(Resource):
+  def get(self):
+
+
+
+    return {
+      'resultStatus': 'SUCCESS',
+      'message': "Success"
+      }
+
